dea0c3985b877d38936e7fdd89dcee054f3e09ba
Javier Sierra <javier.sierra@oc.vasscompany.com>
2025-01-26 11:09:58 +0100

    chore: add -lock.json to gitignore

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,7 +3,6 @@
 
 import { Router } from 'express';
 import { registerUser, loginUser } from '../controllers/authController';
-import { AddFoodEntryComponent } from '../../src/app/components/add-food-entry/add-food-entry.component';
 
 const router = Router();
 
@@ -11,11 +10,3 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 
 export default router;
-
-
-
-
-export const routes: Routes = [
-  { path: 'add-food-entry', component: AddFoodEntryComponent },
-  { path: '', redirectTo: '/add-food-entry', pathMatch: 'full' }
-];
