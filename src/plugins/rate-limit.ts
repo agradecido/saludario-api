deb34cbfdee9894f79b40a9d13d8ed31a91c0f47
Javier Sierra <agradecido@manerasdevivir.com>
2026-03-13 16:42:44 +0100

    feat: add global rate-limit plugin

--- src/plugins/rate-limit.ts
+++ src/plugins/rate-limit.ts
@@ -1,8 +1,15 @@
+import rateLimit from "@fastify/rate-limit";
 import fp from "fastify-plugin";
 import type { FastifyPluginAsync } from "fastify";
 
-const rateLimitPluginImpl: FastifyPluginAsync = async () => {
-  // Implemented in Step 10.
+import { config } from "../config/index.js";
+
+const rateLimitPluginImpl: FastifyPluginAsync = async (fastify) => {
+  await fastify.register(rateLimit, {
+    global: true,
+    max: config.RATE_LIMIT_MAX,
+    timeWindow: config.RATE_LIMIT_WINDOW_MS
+  });
 };
 
 export const rateLimitPlugin = fp(rateLimitPluginImpl, { name: "rate-limit" });
