98d92ae1c6141bc719e659812d2078a32e449efb
Javier Sierra <agradecido@manerasdevivir.com>
2026-03-13 16:42:02 +0100

    feat: implement fastify prisma plugin

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,8 +1,21 @@
 import fp from "fastify-plugin";
 import type { FastifyPluginAsync } from "fastify";
+import { PrismaClient } from "@prisma/client";
 
-const prismaPluginImpl: FastifyPluginAsync = async () => {
-  // Implemented in Step 8.
+declare module "fastify" {
+  interface FastifyInstance {
+    prisma: PrismaClient;
+  }
+}
+
+const prismaPluginImpl: FastifyPluginAsync = async (fastify) => {
+  const prisma = new PrismaClient();
+
+  fastify.decorate("prisma", prisma);
+
+  fastify.addHook("onClose", async () => {
+    await prisma.$disconnect();
+  });
 };
 
 export const prismaPlugin = fp(prismaPluginImpl, { name: "prisma" });
