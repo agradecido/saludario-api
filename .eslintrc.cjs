0c8ef533a50bad0086100c80d5e548ecf29d11a1
Javier Sierra <agradecido@manerasdevivir.com>
2026-03-13 16:33:08 +0100

    chore: initialize node and typescript tooling

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  ignorePatterns: ["dist/", "node_modules/"],
+  rules: {
+    "no-console": "off"
+  }
+};
